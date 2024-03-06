3871ecea9c3d6656e18cb48240e1de5de9beb39f
Summry <nazira.fakhrurradi7@gmail.com>
2024-03-05 20:36:35 -0800

    added a timeout

--- Storage/create_tables_mysql.py
+++ Storage/create_tables_mysql.py
@@ -1,22 +1,32 @@
 import mysql.connector
 import yaml
+import time
 
 with open('app_conf.yml', 'r') as f:
   app_config = yaml.safe_load(f.read())['datastore']
 
 connected = False
 
-while not connected:
-  try:
-    db_conn = mysql.connector.connect(
+# while not connected:
+#   try:
+#     db_conn = mysql.connector.connect(
+#       host=app_config['hostname'],
+#       user=app_config['user'],
+#       password=app_config['password'],
+#       database=app_config['db']
+#     )
+#     connected = True
+#   except:
+#     continue
+
+time.sleep(15)
+
+db_conn = mysql.connector.connect(
       host=app_config['hostname'],
       user=app_config['user'],
       password=app_config['password'],
       database=app_config['db']
     )
-    connected = True
-  except:
-    continue
 
 db_cursor = db_conn.cursor()
 
