9ffcb4dd1228250dd6fa1d6e3a0efe58a930e339
Summry <nazira.fakhrurradi7@gmail.com>
2024-03-16 17:57:31 -0700

    dashboard stuff

---
+++ dashboard/src/app/tim/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Tim() {
+  return (
+    <h1 className="flex justify-center items-center h-screen">
+      Tim diff on god no cap.
+    </h1>
+  );
+}
