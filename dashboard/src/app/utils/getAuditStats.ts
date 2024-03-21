f3d263046923ec8f392449da1dfef6ae46fbc22c
Summry <nazira.fakhrurradi7@gmail.com>
2024-03-21 13:30:47 -0700

    dashboard .env, random audit

--- dashboard/src/app/utils/getAuditStats.ts
+++ dashboard/src/app/utils/getAuditStats.ts
@@ -1,12 +1,28 @@
 import { IAuditDelivery, IAuditSchedule } from "../types/AuditStats";
+import getProcessorStats from "./getProcessorStats";
 
 export default async function getAuditStats() {
   try {
+    const processorStats = await getProcessorStats();
+
+    if (!processorStats) {
+      throw new Error(
+        "There was an error fetching Processor Stats from Audit."
+      );
+    }
+
+    // const randomDeliveryIndex = Math.floor(
+    //   Math.random() * processorStats.num_of_deliveries
+    // );
+    // const randomScheduleIndex = Math.floor(
+    //   Math.random() * processorStats.num_of_schedules
+    // );
+
     const auditDeliveryResponse = await fetch(
-      "http://acit3855lab6a.westus.cloudapp.azure.com:8110/delishery/delivery?index=0"
+      `${process.env.NEXT_PUBLIC_AUDIT_API_URL}/delivery?index=0`
     );
     const auditScheduleResponse = await fetch(
-      "http://acit3855lab6a.westus.cloudapp.azure.com:8110/delishery/schedule?index=0"
+      `${process.env.NEXT_PUBLIC_AUDIT_API_URL}/schedule?index=0`
     );
 
     if (auditDeliveryResponse.status === 404) {
--- dashboard/src/app/utils/getProcessorStats.ts
+++ dashboard/src/app/utils/getProcessorStats.ts
@@ -3,7 +3,7 @@ import { IProcessorStats } from "../types/ProcessorStats";
 export default async function getProcessorStats() {
   try {
     const statsResponse = await fetch(
-      "http://acit3855lab6a.westus.cloudapp.azure.com:8100/stats"
+      `${process.env.NEXT_PUBLIC_PROCESSOR_API_URL}`
     );
 
     if (!statsResponse.ok) {
