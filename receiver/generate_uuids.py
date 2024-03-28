193aa3815ce57d96cec9c85da954160797ea04d6
Nazira Fakhrurradi <nazira.fakhrurradi7@gmail.com>
2024-03-27 23:14:44 -0700

    reformat to pep8 style for pipeline

--- receiver/generate_uuids.py
+++ receiver/generate_uuids.py
@@ -1,4 +1,5 @@
-import uuid, csv
+import uuid
+import csv
 
 """
 The purpose of this file is to help Apache JMeter 
@@ -9,19 +10,19 @@ generate random uuids for the following API properties:
 """
 
 with open('deliveries.csv', 'w', newline='') as fp:
-  write_csv = csv.DictWriter(fp, fieldnames=['delivery_id', 'user_id'])
-  write_csv.writeheader()
-  for i in range(1000):
-    write_csv.writerow({
-      "delivery_id": uuid.uuid4(),
-      "user_id": uuid.uuid4()
-    })
+    write_csv = csv.DictWriter(fp, fieldnames=['delivery_id', 'user_id'])
+    write_csv.writeheader()
+    for i in range(1000):
+        write_csv.writerow({
+            "delivery_id": uuid.uuid4(),
+            "user_id": uuid.uuid4()
+        })
 
 with open('schedules.csv', 'w', newline='') as fp:
-  write_csv = csv.DictWriter(fp, fieldnames=['schedule_id', 'user_id'])
-  write_csv.writeheader()
-  for i in range(1000):
-    write_csv.writerow({
-      "schedule_id": uuid.uuid4(),
-      "user_id": uuid.uuid4()
-    })
+    write_csv = csv.DictWriter(fp, fieldnames=['schedule_id', 'user_id'])
+    write_csv.writeheader()
+    for i in range(1000):
+        write_csv.writerow({
+            "schedule_id": uuid.uuid4(),
+            "user_id": uuid.uuid4()
+        })
--- storage/app.py
+++ storage/app.py
@@ -1,4 +1,9 @@
-import connexion, yaml, logging, logging.config, datetime, json
+import connexion
+import yaml
+import logging
+import logging.config
+import datetime
+import json
 from connexion import NoContent
 
 from sqlalchemy import and_, create_engine
@@ -10,15 +15,17 @@ from base import Base
 from models import Delivery, Schedule
 import time
 
-def configure_app():
-  """Stores log events in the app.log file for every request
 
-  Returns:
-      Dictionary: app configuration details
-  """
-  with open('app_conf.yml', 'r') as f:
-    app_config = yaml.safe_load(f.read())
-  return app_config
+def configure_app():
+    """Stores log events in the app.log file for every request
+
+    Returns:
+        Dictionary: app configuration details
+    """
+    with open('app_conf.yml', 'r') as f:
+        app_config = yaml.safe_load(f.read())
+    return app_config
+
 
 app_config = configure_app()
 
@@ -30,203 +37,222 @@ DB_HOST = db_creds['hostname']
 DB_PORT = db_creds['port']
 DB_NAME = db_creds['db']
 
-DB_ENGINE = create_engine(f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}", pool_pre_ping=True, pool_size=10, pool_recycle=600)
+DB_ENGINE = create_engine(
+    f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}", pool_pre_ping=True, pool_size=10, pool_recycle=600)
 Base.metadata.bind = DB_ENGINE
 DB_SESSION = sessionmaker(bind=DB_ENGINE)
 
+
 def log_db_info():
-  """Logs database connection details
-  """
-  logger = logging.getLogger('basicLogger')
-  logger.info(f"Connecting to DB. Hostname:{DB_HOST}, Port:{DB_PORT}")
+    """Logs database connection details
+    """
+    logger = logging.getLogger('basicLogger')
+    logger.info(f"Connecting to DB. Hostname:{DB_HOST}, Port:{DB_PORT}")
+
 
 log_db_info()
 
+
 def configure_logging():
-  """Logging configuration - creates app.log file
-  """
-  with open('log_conf.yml', 'r') as f:
-    log_config = yaml.safe_load(f.read())
-    logging.config.dictConfig(log_config)
+    """Logging configuration - creates app.log file
+    """
+    with open('log_conf.yml', 'r') as f:
+        log_config = yaml.safe_load(f.read())
+        logging.config.dictConfig(log_config)
+
 
 def log_post_info(event, trace_id):
-  """Logs requests
+    """Logs requests
 
-  Args:
-      event (string): event type
-      trace_id (string): event tracer
-  """
-  logger = logging.getLogger('basicLogger')
-  logger.info(f"Stored event {event} request with a trace id of {trace_id}")
+    Args:
+        event (string): event type
+        trace_id (string): event tracer
+    """
+    logger = logging.getLogger('basicLogger')
+    logger.info(f"Stored event {event} request with a trace id of {trace_id}")
+
 
 def log_get_info(event, start_timestamp, len_of_results):
-  """Logs GET requests for each event
+    """Logs GET requests for each event
 
-  Args:
-      event (string): Delivery or Schedule
-      start_timestamp (string): Datetime URL query parameter
-      len_of_results (int): length of the results list
-  """
-  logger = logging.getLogger('basicLogger')
-  logger.info(f"Query for {event} after {start_timestamp} returns {len_of_results} results")
+    Args:
+        event (string): Delivery or Schedule
+        start_timestamp (string): Datetime URL query parameter
+        len_of_results (int): length of the results list
+    """
+    logger = logging.getLogger('basicLogger')
+    logger.info(
+        f"Query for {event} after {start_timestamp} returns {len_of_results} results")
+
 
 def write_to_database(message):
-  """This function will write the body/data/payload to the database
+    """This function will write the body/data/payload to the database
 
-  Args:
-      Kafka Message (dict): message obtained from kafka
-  """
+    Args:
+        Kafka Message (dict): message obtained from kafka
+    """
 
-  session = DB_SESSION()
+    session = DB_SESSION()
 
-  evt = {}
-  event_type = message['type']
-  body = message['payload']
+    evt = {}
+    event_type = message['type']
+    body = message['payload']
 
-  if event_type == "delivery":
-    evt = Delivery(
-      delivery_id = body['delivery_id'],
-      user_id = body['user_id'],
-      item_quantity = body['item_quantity'],
-      trace_id = body['trace_id']
-    )
+    if event_type == "delivery":
+        evt = Delivery(
+            delivery_id=body['delivery_id'],
+            user_id=body['user_id'],
+            item_quantity=body['item_quantity'],
+            trace_id=body['trace_id']
+        )
 
-  elif event_type == "schedule":
-    evt = Schedule(
-      schedule_id = body['schedule_id'],
-      user_id = body['user_id'],
-      number_of_deliveries = body['number_of_deliveries'],
-      trace_id = body['trace_id']
-    )
+    elif event_type == "schedule":
+        evt = Schedule(
+            schedule_id=body['schedule_id'],
+            user_id=body['user_id'],
+            number_of_deliveries=body['number_of_deliveries'],
+            trace_id=body['trace_id']
+        )
 
-  session.add(evt)
+    session.add(evt)
 
-  session.commit()
-  session.close()
+    session.commit()
+    session.close()
 
-  log_post_info(event_type, body['trace_id'])
+    log_post_info(event_type, body['trace_id'])
+
 
 def get_deliveries(start_timestamp, end_timestamp):
-  """GET request that gets list of deliveries
+    """GET request that gets list of deliveries
 
-  Args:
-      start_timestamp (string): lower boundary of timestampts
-      end_timestamp (string): upper boundary of timestamps
+    Args:
+        start_timestamp (string): lower boundary of timestampts
+        end_timestamp (string): upper boundary of timestamps
 
-  Returns:
-      JSON: json array of items
-  """
-  session = DB_SESSION()
+    Returns:
+        JSON: json array of items
+    """
+    session = DB_SESSION()
 
-  start_timestamp_datetime = datetime.datetime.strptime(start_timestamp, "%Y-%m-%d %H:%M:%S")
-  end_timestamp_datetime = datetime.datetime.strptime(end_timestamp, "%Y-%m-%d %H:%M:%S")
+    start_timestamp_datetime = datetime.datetime.strptime(
+        start_timestamp, "%Y-%m-%d %H:%M:%S")
+    end_timestamp_datetime = datetime.datetime.strptime(
+        end_timestamp, "%Y-%m-%d %H:%M:%S")
 
-  results = session.query(Delivery).filter(
-    and_(
-      Delivery.requested_date > start_timestamp_datetime,
-      Delivery.requested_date <= end_timestamp_datetime
-    )
-  ).all()
+    results = session.query(Delivery).filter(
+        and_(
+            Delivery.requested_date > start_timestamp_datetime,
+            Delivery.requested_date <= end_timestamp_datetime
+        )
+    ).all()
 
-  results_list = []
+    results_list = []
 
-  for result in results:
-    results_list.append(result.to_dict())
-    
-  session.close()
+    for result in results:
+        results_list.append(result.to_dict())
 
-  log_get_info('deliveries', start_timestamp, len(results_list))
-  
-  return results_list, 200
+    session.close()
+
+    log_get_info('deliveries', start_timestamp, len(results_list))
+
+    return results_list, 200
+
 
 def get_schedules(start_timestamp, end_timestamp):
-  """GET request that gets list of schedules
+    """GET request that gets list of schedules
 
-  Args:
-      start_timestamp (string): URL query parameter
-      end_timestamp (string): URL query parameter
-  """
-  session = DB_SESSION()
+    Args:
+        start_timestamp (string): URL query parameter
+        end_timestamp (string): URL query parameter
+    """
+    session = DB_SESSION()
 
-  start_timestamp_datetime = datetime.datetime.strptime(start_timestamp, "%Y-%m-%d %H:%M:%S")
-  end_timestamp_datetime = datetime.datetime.strptime(end_timestamp, "%Y-%m-%d %H:%M:%S")
+    start_timestamp_datetime = datetime.datetime.strptime(
+        start_timestamp, "%Y-%m-%d %H:%M:%S")
+    end_timestamp_datetime = datetime.datetime.strptime(
+        end_timestamp, "%Y-%m-%d %H:%M:%S")
 
-  results = session.query(Schedule).filter(
-    and_(
-      Schedule.created_date > start_timestamp_datetime,
-      Schedule.created_date <= end_timestamp_datetime
-    )
-  ).all()
+    results = session.query(Schedule).filter(
+        and_(
+            Schedule.created_date > start_timestamp_datetime,
+            Schedule.created_date <= end_timestamp_datetime
+        )
+    ).all()
 
-  results_list = []
+    results_list = []
 
-  for result in results:
-    results_list.append(result.to_dict())
+    for result in results:
+        results_list.append(result.to_dict())
 
-  session.close()
+    session.close()
 
-  log_get_info('schedules', start_timestamp, len(results_list))
+    log_get_info('schedules', start_timestamp, len(results_list))
 
-  return results_list, 200
+    return results_list, 200
+
 
 def process_messages():
-  """ Process event messages """
-  # hostname = "%s:%d" % (app_config['events']['hostname'], app_config['events']['port'])
-  # client = KafkaClient(hosts=hostname)
-  # topic = client.topics[str.encode(app_config['events']['topic'])]
+    """ Process event messages """
+    # hostname = "%s:%d" % (app_config['events']['hostname'], app_config['events']['port'])
+    # client = KafkaClient(hosts=hostname)
+    # topic = client.topics[str.encode(app_config['events']['topic'])]
 
-  max_retries = app_config['events']['max_retries']
-  retry_count = 0
-  logger = logging.getLogger('basicLogger')
+    max_retries = app_config['events']['max_retries']
+    retry_count = 0
+    logger = logging.getLogger('basicLogger')
 
-  while retry_count < max_retries:
-    try:
-      hostname = "%s:%d" % (app_config['events']['hostname'], app_config['events']['port'])
-      client = KafkaClient(hosts=hostname)
-      topic = client.topics[str.encode(app_config['events']['topic'])]
-      consumer = topic.get_simple_consumer(consumer_group=b'event_group', 
-                                         reset_offset_on_start=False, 
-                                         auto_offset_reset=OffsetType.LATEST)
-      for msg in consumer:
-        msg_str = msg.value.decode('utf-8')
-        msg = json.loads(msg_str)
-        logger.info("Message: %s" % msg)
+    while retry_count < max_retries:
+        try:
+            hostname = "%s:%d" % (
+                app_config['events']['hostname'], app_config['events']['port'])
+            client = KafkaClient(hosts=hostname)
+            topic = client.topics[str.encode(app_config['events']['topic'])]
+            consumer = topic.get_simple_consumer(consumer_group=b'event_group',
+                                                 reset_offset_on_start=False,
+                                                 auto_offset_reset=OffsetType.LATEST)
+            for msg in consumer:
+                msg_str = msg.value.decode('utf-8')
+                msg = json.loads(msg_str)
+                logger.info("Message: %s" % msg)
 
-        write_to_database(msg)
+                write_to_database(msg)
 
-        consumer.commit_offsets()
+                consumer.commit_offsets()
 
-    except Exception as e:
-      logger.error(f"Failed to connect to Kafka: {str(e)}")
-      logger.info(f"Retrying to connect to Kafka. Retry count: {retry_count}")
-      time.sleep(app_config['events']['retry_interval'])
-      
-      retry_count += 1
+        except Exception as e:
+            logger.error(f"Failed to connect to Kafka: {str(e)}")
+            logger.info(
+                f"Retrying to connect to Kafka. Retry count: {retry_count}")
+            time.sleep(app_config['events']['retry_interval'])
 
-  logger.error(f"Failed to connect to Kafka after {max_retries} retries. Exiting now.")
+            retry_count += 1
 
-  # consumer = topic.get_simple_consumer(consumer_group=b'event_group', 
-  #                                      reset_offset_on_start=False, 
-  #                                      auto_offset_reset=OffsetType.LATEST)
-  
-  # for msg in consumer:
-  #   msg_str = msg.value.decode('utf-8')
-  #   msg = json.loads(msg_str)
-  #   logger = logging.getLogger('basicLogger')
-  #   logger.info("Message: %s" % msg)
+    logger.error(
+        f"Failed to connect to Kafka after {max_retries} retries. Exiting now.")
 
-  #   write_to_database(msg)
+    # consumer = topic.get_simple_consumer(consumer_group=b'event_group',
+    #                                      reset_offset_on_start=False,
+    #                                      auto_offset_reset=OffsetType.LATEST)
 
-  #   consumer.commit_offsets()
+    # for msg in consumer:
+    #   msg_str = msg.value.decode('utf-8')
+    #   msg = json.loads(msg_str)
+    #   logger = logging.getLogger('basicLogger')
+    #   logger.info("Message: %s" % msg)
+
+    #   write_to_database(msg)
+
+    #   consumer.commit_offsets()
+
 
 app = connexion.FlaskApp(__name__, specification_dir='')
 app.add_api("delishery.yaml", strict_validation=True, validate_responses=True)
 
 if __name__ == "__main__":
-  configure_logging()
+    configure_logging()
 
-  t1 = Thread(target=process_messages)
-  t1.setDaemon(True)
-  t1.start()
+    t1 = Thread(target=process_messages)
+    t1.setDaemon(True)
+    t1.start()
 
-  app.run(port=app_config['app']['port'], host='0.0.0.0')
+    app.run(port=app_config['app']['port'], host='0.0.0.0')
