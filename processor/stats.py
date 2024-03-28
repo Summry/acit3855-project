562874461d1e7c5adb3bf2e4b308cdba1f93aadc
Ubuntu <azureuser@Lab6AKafka.a45s0j4ffryeljc4sxe1q5sxed.dx.internal.cloudapp.net>
2024-03-28 07:26:43 +0000

    git

---
+++ processor/stats.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+
+from sqlalchemy import DateTime, Integer
+from sqlalchemy.orm import DeclarativeBase, mapped_column
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class Stats(Base):
+    __tablename__ = 'delishery_stats'
+
+    id = mapped_column(Integer, primary_key=True, autoincrement=True)
+    num_of_deliveries = mapped_column(Integer, nullable=False)
+    num_of_schedules = mapped_column(Integer, nullable=False)
+    total_delivery_items = mapped_column(Integer, nullable=False)
+    total_scheduled_deliveries = mapped_column(Integer, nullable=False)
+    last_updated = mapped_column(
+        DateTime, nullable=False, default=lambda: datetime.now())
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'num_of_deliveries': self.num_of_deliveries,
+            'num_of_schedules': self.num_of_schedules,
+            'total_delivery_items': self.total_delivery_items,
+            'total_scheduled_deliveries': self.total_scheduled_deliveries,
+            'last_updated': self.last_updated
+        }
--- storage/drop_tables_mysql.py
+++ storage/drop_tables_mysql.py
@@ -2,13 +2,13 @@ import mysql.connector
 import yaml
 
 with open('app_conf.yml', 'r') as f:
-  app_config = yaml.safe_load(f.read())['datastore']
+    app_config = yaml.safe_load(f.read())['datastore']
 
 db_conn = mysql.connector.connect(
-  host=app_config['hostname'],
-  user=app_config['user'],
-  password=app_config['password'],
-  database=app_config['db']
+    host=app_config['hostname'],
+    user=app_config['user'],
+    password=app_config['password'],
+    database=app_config['db']
 )
 
 db_cursor = db_conn.cursor()
